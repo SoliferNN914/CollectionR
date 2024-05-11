d5576e83181dc61504848660d5b3e57bd6d5d14c
SoliferNN914 <oskarson16@gmail.com>
2024-05-11 15:48:06 +0100

    fixed navigation

--- app/App.jsx
+++ app/App.jsx
@@ -1,31 +1,28 @@
-import React from 'react'
-import HomePage from '../components/HomePage'
-import { NavigationContainer } from '@react-navigation/native'
-import { Stack } from 'expo-router'
-import ListItem from '../components/ListItem'
-import List from '../components/List'
+import React from "react";
+import HomePage from "../components/HomePage";
+//import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import ListItem from "../components/ListItem";
+import List from "../components/List";
 
-export default function App(){
-    return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen
-                name="HomePage"
-                component={HomePage}
-                options={{title: 'Home Page'}}
-                />
-                <Stack.Screen
-                name="ListItem"
-                component={ListItem}
-                options={{title: 'List Item'}}
-                />
-                <Stack.Screen
-                name="List"
-                component={List}
-                options={{title: 'List'}}
-                />
-            </Stack.Navigator>
-        </NavigationContainer>
+const Stack = createStackNavigator();
 
-    )
+export default function App() {
+  return (
+
+      <Stack.Navigator>
+        <Stack.Screen
+          name="HomePage"
+          component={HomePage}
+          options={{ title: "Home Page" }}
+        />
+        <Stack.Screen
+          name="ListItem"
+          component={ListItem}
+          options={{ title: "List Item" }}
+        />
+        <Stack.Screen name="List" component={List} options={{ title: "List" }} />
+      </Stack.Navigator>
+
+  );
 }
--- components/List.jsx
+++ components/List.jsx
@@ -9,16 +9,13 @@ const Data = [
   // Add more items as needed
 ];
 
-export default function List({ navigation }) {
-
-//   const handlePress = (itemId, title) => {
-//     navigation.navigate("ListItem", { itemId, title });
-//   };
+export default function List() {
+  const navigation = useNavigation();
 
   return (
     <View className="flex-1 items-center justify-center">
       {Data.map(item => (
-        <TouchableOpacity key={item.id} className="p-4 m-2 bg-gray-200 rounded" onPress={() => navigation.navigate('ListItem', { itemId: item.id, title: item.title })}>
+        <TouchableOpacity key={item.id} className="p-4 m-2 bg-green-800 rounded" onPress={() => navigation.navigate('ListItem', { itemId: item.id, title: item.title })}>
           <Text className="text-lg">{item.title}</Text>
         </TouchableOpacity>
       ))}
